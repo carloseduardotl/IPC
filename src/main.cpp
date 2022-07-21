18d21401dac2717315f717f63c3c96b93a77bcce
Carlos Eduardo Taborda Lottermann <cadutaborda@gmail.com>
2022-07-20 21:21:11 -0300

    Bathroom tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Function_pool.h"
+#include "Bathroom.h"
 #include <cstdlib>
 #include <functional>
 #include <string>
@@ -8,7 +9,7 @@
 #include <thread>
 #include <vector>
 
-enum state
+/*enum state
 {
     male,
     female,
@@ -22,8 +23,6 @@ typedef struct bathroom
     std::vector<std::thread> thread_pool;
     state current;
 } bathroom;
-
-//Function_pool func_pool;
 
 void clear_bathroom(bathroom * room)
 {
@@ -46,16 +45,7 @@ void spawn(bathroom * room, std::function<void()> func)
     for(int i=0; i< female; i++) room->female_queue.push(func);
 
     std::cout << "Spawned " << male << " men and " << female << " women" << std::endl;
-}
-
-void example_function()
-{
-    int ran;
-    ran = rand()%10;
-    //std::cout << "Person entered the bathroom, using for " << ran << " seconds" << std::endl;
-    //std::this_thread::sleep_for(std::chrono::seconds(ran));
-    std::cout << "Person exited" << std::endl;
-}
+}*/
 
 void workload()
 {
@@ -68,39 +58,21 @@ void workload()
 
 int main()
 {
-    bathroom room;
+    //bathroom room;
     std::cout << "stating operation" << std::endl;
-    int num_threads = 5; //std::thread::hardware_concurrency();
+    int num_threads = 5;
     std::cout << "number of threads = " << num_threads << std::endl;
-    //std::vector<std::thread> thread_pool;
-    /*for (int i = 0; i < 50; i++)
-    {
-        room.male_queue.push(example_function);
-        //std::cout << "queue line: " << func_pool.get_queue_size() << std::endl;
-    }*/
-    spawn(&room, workload);
+    /*spawn(&room, workload);
     for (int i = 0; i < num_threads; i++)
     {
         room.thread_pool.push_back(std::thread(&Function_pool::infinite_loop_func, &room.male_queue));
     }
-    //here we should send our functions
-    clear_bathroom(&room);
-    /*
-    room.male_queue.restart();
-    room.male_queue.push(nunca_executar);
-    room.male_queue.push(nunca_executar);
-    room.male_queue.push(nunca_executar);
-    room.male_queue.push(nunca_executar);
-    room.male_queue.push(nunca_executar);
-    std::cout << "Thread pool size: " << room.thread_pool.size() << std::endl;
-    std::cout << "queue line: " << room.male_queue.get_queue_size() << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    for (int i = 0; i < 5; i++)
-    {
-        room.thread_pool.push_back(std::thread(&Function_pool::infinite_loop_func, &room.male_queue));
-    }
-    std::cout << "Thread pool size: " << room.thread_pool.size() << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    clear_bathroom(&room);
-    */
+    clear_bathroom(&room);*/
+
+    Bathroom bathroom(num_threads);
+
+    bathroom.spawn(workload);
+    bathroom.start_bathroom();
+    bathroom.clear_bathroom();
+
 }
